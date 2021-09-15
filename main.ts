a7d4649240f4a2b3c5345b6c6c1a8a39312ec931
Joel Peter Anderson <mrklingon@users.noreply.github.com>
2021-09-15 18:46:16 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -9,9 +9,19 @@ function showUNI (xx: number, yy: number) {
     }
 }
 function buildWorld () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    x = 0
+    y = 0
     for (let index = 0; index <= Unisize; index++) {
         Universe[index] = 0
     }
+    basic.pause(500)
     for (let index2 = 0; index2 <= Diameter - 1; index2++) {
         Universe[getSPOT(index2, 4)] = DIRT
         if (6 < randint(0, 10)) {
@@ -23,21 +33,54 @@ function buildWorld () {
     }
 }
 input.onButtonPressed(Button.A, function () {
-    x += -1
-    if (x < 0) {
-        x = 99
+    if (mode == 0) {
+        x += -1
+        if (x < 0) {
+            x = 99
+        }
+        showUNI(x, y)
     }
-    showUNI(x, y)
+    if (mode == 5) {
+        y += -1
+        if (y < 0) {
+            y = 9
+        }
+        showUNI(x, y)
+    }
+    if (mode == 1) {
+        mkTree(x, 4)
+        showUNI(x, y)
+    }
 })
 input.onButtonPressed(Button.AB, function () {
-    buildWorld()
+    mode += 1
+    if (5 < mode) {
+        mode = 0
+    }
+    basic.showString("" + (Modes[mode]))
 })
 input.onButtonPressed(Button.B, function () {
-    x += 1
-    if (x > 99) {
-        x = 0
+    if (mode == 0) {
+        x += 1
+        if (x > 99) {
+            x = 0
+        }
+        showUNI(x, y)
     }
-    showUNI(x, y)
+    if (mode == 5) {
+        y += 1
+        if (y > 9) {
+            y = 0
+        }
+        showUNI(x, y)
+    }
+    if (mode == 1) {
+        mkTree(x + 2, 4)
+        showUNI(x, y)
+    }
+})
+input.onGesture(Gesture.Shake, function () {
+    buildWorld()
 })
 function getY (spot: number) {
     return Math.trunc(spot / Diameter)
@@ -61,12 +104,23 @@ let x = 0
 let GRASS = 0
 let Tree = 0
 let DIRT = 0
+let Modes: string[] = []
 let Universe: number[] = []
 let Unisize = 0
 let Diameter = 0
+let mode = 0
+mode = 0
 Diameter = 100
 Unisize = Diameter * 10
 Universe = [0]
+Modes = [
+"M",
+"T",
+"S",
+"C",
+"R",
+"D"
+]
 for (let index = 0; index < Unisize; index++) {
     Universe.push(0)
 }
@@ -75,5 +129,20 @@ Tree = 100
 GRASS = 100
 x = 0
 y = 0
+images.createBigImage(`
+    . . . . . . . . . .
+    . # # . . . . . . .
+    . # . . . . . . . .
+    . # . . . . . . . .
+    # # # # # # # # # #
+    `).scrollImage(1, 200)
+basic.showString("MyCraft")
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
 buildWorld()
 showUNI(x, y)
