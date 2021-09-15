6b95201d4a280cca4715323bf3f32825d727f160
Joel Peter Anderson <mrklingon@users.noreply.github.com>
2021-09-15 17:51:54 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
+function setPix (xp: number, xy: number, color: number) {
+    Universe[getSPOT(xp, xy)] = color
+}
 function showUNI (xx: number, yy: number) {
     for (let index = 0; index <= 4; index++) {
         for (let index2 = 0; index2 <= 4; index2++) {
@@ -11,6 +14,9 @@ function buildWorld () {
         if (6 < randint(0, 10)) {
             Universe[getSPOT(index2, 3)] = GRASS
         }
+        if (3 == randint(0, 5)) {
+            mkTree(index2, 3)
+        }
     }
 }
 input.onButtonPressed(Button.A, function () {
@@ -19,6 +25,9 @@ input.onButtonPressed(Button.A, function () {
         x = 99
     }
     showUNI(x, y)
+})
+input.onButtonPressed(Button.AB, function () {
+    buildWorld()
 })
 input.onButtonPressed(Button.B, function () {
     x += 1
@@ -30,6 +39,14 @@ input.onButtonPressed(Button.B, function () {
 function getY (spot: number) {
     return Math.trunc(spot / Diameter)
 }
+function mkTree (xt: number, yt: number) {
+    setPix(xt, yt, Tree)
+    setPix(xt, yt - 1, Tree)
+    setPix(xt, yt - 2, Tree)
+    setPix(xt - 1, yt - 2, Tree)
+    setPix(xt, yt - 3, Tree)
+    setPix(xt + 1, yt - 3, Tree)
+}
 function getSPOT (xx: number, yy: number) {
     return xx + yy * Diameter
 }
@@ -39,6 +56,7 @@ function getX (spot: number) {
 let y = 0
 let x = 0
 let GRASS = 0
+let Tree = 0
 let DIRT = 0
 let Universe: number[] = []
 let Diameter = 0
@@ -49,7 +67,7 @@ for (let index = 0; index < Unisize; index++) {
     Universe.push(0)
 }
 DIRT = 255
-let Tree = 100
+Tree = 100
 GRASS = 100
 x = 0
 y = 0
